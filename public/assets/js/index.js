0f1f3fe08ddd18e16bb07e2aa025428aabb2bfb1
cmadrid48 <cmadrid48@gmail.com>
2023-03-07 15:47:37 -0800

    index and notes html input, working on index

---
+++ public/assets/js/index.js
@@ -0,0 +1,21 @@
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
+
+if (window.location.pathname === '/notes') {
+    noteTitle = document.querySelector('.note-title');
+    noteText = document.querySelector('.note-textarea');
+    saveNoteBtn = document.querySelector('.save-note');
+    newNoteBtn = document.querySelector('.new-note');
+    noteList = document.querySelector('list-container .list-group');
+};
+
+const show = (elem) => {
+    elem.style.display = 'inline';
+};
+
+const hide = (elem) => {
+    elem.style.display = 'none';
+};
